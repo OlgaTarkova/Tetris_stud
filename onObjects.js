074e36c78877b2327c4ad4bb4b73b1d76317a370
olga <tarkova.o@yandex.ru>
2018-05-12 20:44:12 +0300

    ///

--- onObjects.js
+++ onObjects.js
@@ -1,21 +1,31 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Field = (function () {
     function Field() {
-        this.FIELD_WIDTH = 12;
-        this.FIELD_HEIGHT = 25;
+        this.fieldWidth = 12;
+        this.fieldHeight = 25;
         this.elField = document.getElementById("field");
         this.data = [];
-        for (var x = 0; x < this.FIELD_WIDTH; x++) {
+        for (var x = 0; x < this.fieldWidth; x++) {
             var fieldCol = [];
-            for (var y = 0; y < this.FIELD_HEIGHT; y++) {
-                fieldCol.push((x == 0) || (x == this.FIELD_WIDTH - 1) || (y == this.FIELD_HEIGHT - 1));
+            for (var y = 0; y < this.fieldHeight; y++) {
+                fieldCol.push((x == 0) || (x == this.fieldWidth - 1) || (y == this.fieldHeight - 1));
             }
             this.data.push(fieldCol);
         }
     }
     Field.prototype.draw = function () {
         var html = "";
-        for (var y = 0; y < this.FIELD_HEIGHT; y++) {
-            for (var x = 0; x < this.FIELD_WIDTH; x++) {
+        for (var y = 0; y < this.fieldHeight; y++) {
+            for (var x = 0; x < this.fieldWidth; x++) {
                 html += this.data[x][y] ? "*" : " ";
             }
             html += "<br>";
@@ -24,5 +34,95 @@ var Field = (function () {
     };
     return Field;
 }());
-var field = new Field();
-field.draw();
+var Figure = (function () {
+    function Figure() {
+    }
+    return Figure;
+}());
+var FigureSquare = (function (_super) {
+    __extends(FigureSquare, _super);
+    function FigureSquare() {
+        var _this = _super.call(this) || this;
+        _this.field[_this.figX][_this.figY] = _this.drawOrClear;
+        _this.field[_this.figX + 1][_this.figY] = _this.drawOrClear;
+        _this.field[_this.figX][_this.figY + 1] = _this.drawOrClear;
+        _this.field[_this.figX + 1][_this.figY + 1] = _this.drawOrClear;
+        return _this;
+    }
+    return FigureSquare;
+}(Figure));
+var FigureHorizontal = (function (_super) {
+    __extends(FigureHorizontal, _super);
+    function FigureHorizontal() {
+        var _this = _super.call(this) || this;
+        _this.field[_this.figX][_this.figY] = _this.drawOrClear;
+        _this.field[_this.figX + 1][_this.figY] = _this.drawOrClear;
+        _this.field[_this.figX + 2][_this.figY] = _this.drawOrClear;
+        _this.field[_this.figX + 3][_this.figY] = _this.drawOrClear;
+        return _this;
+    }
+    return FigureHorizontal;
+}(Figure));
+var FigureZ = (function (_super) {
+    __extends(FigureZ, _super);
+    function FigureZ() {
+        var _this = _super.call(this) || this;
+        _this.field[_this.figX][_this.figY] = _this.drawOrClear;
+        _this.field[_this.figX + 1][_this.figY] = _this.drawOrClear;
+        _this.field[_this.figX + 1][_this.figY + 1] = _this.drawOrClear;
+        _this.field[_this.figX + 2][_this.figY + 1] = _this.drawOrClear;
+        return _this;
+    }
+    return FigureZ;
+}(Figure));
+var FigureL = (function (_super) {
+    __extends(FigureL, _super);
+    function FigureL() {
+        var _this = _super.call(this) || this;
+        _this.field[_this.figX][_this.figY] = _this.drawOrClear;
+        _this.field[_this.figX + 1][_this.figY] = _this.drawOrClear;
+        _this.field[_this.figX + 2][_this.figY] = _this.drawOrClear;
+        _this.field[_this.figX + 2][_this.figY + 1] = _this.drawOrClear;
+        return _this;
+    }
+    return FigureL;
+}(Figure));
+var Game = (function () {
+    function Game() {
+        this.field = new Field();
+        //this.currentFigure = this.newFigure();
+    }
+    Game.prototype.draw = function () {
+        this.field.draw();
+        this.currentFigure = this.newFigure();
+        //this.figX = this.field.fieldWidth / 2 - 2;
+        //this.figY = 1;
+    };
+    Game.prototype.newFigure = function () {
+        var randomFigure = Math.floor(Math.random() * 4);
+        var newFigure;
+        this.newFigure.figX = this.field.fieldWidth / 2 - 2;
+        //this.newFigure.figY = 1;
+        switch (randomFigure) {
+            case 0: {
+                newFigure = new FigureSquare();
+                break;
+            }
+            case 1: {
+                newFigure = new FigureHorizontal();
+                break;
+            }
+            case 2: {
+                newFigure = new FigureZ();
+                break;
+            }
+            case 3: {
+                newFigure = new FigureL();
+            }
+        }
+        return newFigure;
+    };
+    return Game;
+}());
+var game = new Game();
+game.draw();
--- onObjects.ts
+++ onObjects.ts
@@ -1,18 +1,16 @@
-class Field{
-    readonly FIELD_WIDTH = 12;
-    readonly FIELD_HEIGHT = 25;
+class Field {
+    readonly fieldWidth = 12;
+    readonly fieldHeight = 25;
     protected data: boolean[][];
-    //protected figX: number;
-    //protected figY: number;
-    protected elField:HTMLElement;
+    protected elField: HTMLElement;
 
     constructor() {
         this.elField = document.getElementById("field");
         this.data = [];
-        for(let x = 0; x < this.FIELD_WIDTH; x++) {
+        for (let x = 0; x < this.fieldWidth; x++) {
             let fieldCol : boolean[] = [];
-            for(let y = 0; y < this.FIELD_HEIGHT; y++) {
-                fieldCol.push((x == 0) || (x == this.FIELD_WIDTH - 1) || (y == this.FIELD_HEIGHT - 1));
+            for(let y = 0; y < this.fieldHeight; y++) {
+                fieldCol.push((x == 0) || (x == this.fieldWidth - 1) || (y == this.fieldHeight - 1));
             }
             this.data.push(fieldCol);
         }
@@ -20,15 +18,118 @@ class Field{
 
     public draw() {
         let html: string = "";
-        for(let y = 0; y < this.FIELD_HEIGHT; y++){
-            for(let x = 0; x < this.FIELD_WIDTH; x++){
+        for (let y = 0; y < this.fieldHeight; y++) {
+            for(let x = 0; x < this.fieldWidth; x++) {
                 html += this.data[x][y]? "*" : " ";
             }
             html+= "<br>";
         }
         this.elField.innerHTML = html;
     }
+
 }
 
-let field:Field = new Field();
-field.draw();
+class Figure {
+    protected figX: number;
+    protected figY: number;
+    protected field: Field;
+    protected drawOrClear: boolean;
+    setPosition: number;
+    protected figType : number;
+
+    constructor(){}
+
+}
+class FigureSquare extends Figure {
+    constructor(){
+        super();
+        this.field[this.figX][this.figY] = this.drawOrClear;
+        this.field[this.figX + 1][this.figY] = this.drawOrClear;
+        this.field[this.figX][this.figY + 1] = this.drawOrClear;
+        this.field[this.figX + 1][this.figY + 1] = this.drawOrClear;
+
+    }
+}
+class FigureHorizontal extends Figure {
+    constructor() {
+        super();
+        this.field[this.figX][this.figY] = this.drawOrClear;
+        this.field[this.figX + 1][this.figY] = this.drawOrClear;
+        this.field[this.figX + 2][this.figY] = this.drawOrClear;
+        this.field[this.figX + 3][this.figY] = this.drawOrClear;
+    }
+}
+class FigureZ extends Figure {
+    constructor(){
+        super();
+        this.field[this.figX][this.figY] = this.drawOrClear;
+        this.field[this.figX + 1][this.figY] = this.drawOrClear;
+        this.field[this.figX + 1][this.figY + 1] = this.drawOrClear;
+        this.field[this.figX + 2][this.figY + 1] = this.drawOrClear;
+    }
+}
+class FigureL extends Figure {
+    constructor(){
+        super();
+        this.field[this.figX][this.figY] = this.drawOrClear;
+        this.field[this.figX + 1][this.figY] = this.drawOrClear;
+        this.field[this.figX + 2][this.figY] = this.drawOrClear;
+        this.field[this.figX + 2][this.figY + 1] = this.drawOrClear;
+    }
+}
+
+
+class Game {
+
+    private currentFigure: Figure;
+    private field: Field;
+
+    constructor(){
+        this.field = new Field();
+        //this.currentFigure = this.newFigure();
+    }
+
+    public draw(){
+        this.field.draw();
+
+        this.currentFigure = this.newFigure();
+        //this.figX = this.field.fieldWidth / 2 - 2;
+        //this.figY = 1;
+    }
+
+    private newFigure(): Figure{
+        let randomFigure = Math.floor(Math.random() * 4);
+
+        let newFigure: Figure;
+        this.newFigure.figX = this.field.fieldWidth / 2 - 2;
+        //this.newFigure.figY = 1;
+
+        switch(randomFigure) {
+
+            case 0: {
+                newFigure = new FigureSquare();
+                break;
+            }
+            case 1: {
+                newFigure = new FigureHorizontal();
+                break;
+            }
+            case 2: {
+                newFigure = new FigureZ();
+                break;
+            }
+            case 3: {
+                newFigure = new FigureL();
+            }
+
+        }
+        return newFigure;
+    }
+
+
+
+}
+
+let game: Game = new Game();
+game.draw();
+
